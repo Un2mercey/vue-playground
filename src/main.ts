59f2fefa1a791c77088c6dbb02b9c62beff3607f
Arseniy Markov <zamberg42@gmail.com>
2024-03-29 18:04:14 +0300

    project setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import setupComponents from './components/global';
 import router from './router';
 import './styles/index.scss';
 
 const app = createApp(App);
-
+setupComponents(app);
 app.use(router).mount('#app');
