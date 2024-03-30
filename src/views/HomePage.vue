654eee7ec5cfade88eb2205c81bd78032a746918
Arseniy Markov <zamberg42@gmail.com>
2024-03-30 04:12:25 +0300

    HomePage title

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,16 +1,30 @@
 <template>
     <PageLayout>
-        <SVGIcon
-            name="chevron-right"
-            fill="red"
-            :width="Sizes.XXL"
-            :height="Sizes.XXL"
-        />
+        <grid-row>
+            <grid-col>
+                <h1>Playground</h1>
+            </grid-col>
+        </grid-row>
     </PageLayout>
 </template>
 
 <script setup lang="ts">
-import { Sizes } from '@/@enums';
-import SVGIcon from '@/components/icons/SVGIcon.vue';
+import GridCol from '@/components/layout/grid/GridCol.vue';
+import GridRow from '@/components/layout/grid/GridRow.vue';
 import PageLayout from '@/components/layout/structural/PageLayout.vue';
 </script>
+
+<style lang="scss" scoped>
+h1 {
+    background: -webkit-linear-gradient(315deg, #42d392 25%, #647eff);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: 64px;
+    line-height: 1.25;
+    font-weight: 700;
+    letter-spacing: -1.5px;
+    margin: 0;
+    text-align: center;
+}
+</style>
