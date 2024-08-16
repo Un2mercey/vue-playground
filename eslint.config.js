407554a963ffb30bb4fa136e8e54c9c6c861a89a
Arseniy Markov <zamberg42@gmail.com>
2024-08-16 12:24:15 +0300

    eslint 9.9

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import { includeIgnoreFile } from '@eslint/compat';
+import pluginJs from '@eslint/js';
+import configPrettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import tseslint from 'typescript-eslint';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const gitignorePath = resolve(__dirname, '.gitignore');
+
+export default [
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...pluginVue.configs['flat/recommended'],
+    includeIgnoreFile(gitignorePath),
+    {
+        files: ['**/*.{ts,vue}'],
+        ignores: ['node_modules/**/*', 'dist/**/*'],
+        languageOptions: {
+            globals: globals.browser,
+            parserOptions: {
+                parser: tseslint.parser,
+            },
+        },
+        plugins: {
+            pluginPrettier,
+        },
+        rules: {
+            ...configPrettier.rules,
+        },
+    },
+];
